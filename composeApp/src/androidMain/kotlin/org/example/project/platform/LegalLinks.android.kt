e67714c6f3ede1c99d0cbc03905aae34d2daba56
Artem280590 <gonya90.ag@gmail.com>
2026-05-25 15:05:00 +0300

    add path fix 2

--- composeApp/src/androidMain/kotlin/org/example/project/platform/LegalLinks.android.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/platform/LegalLinks.android.kt
@@ -3,6 +3,6 @@ package org.example.project.platform
 actual fun platformLegalLinks(): List<LegalLink> = listOf(
     LegalLink(
         title = "Privacy Policy",
-        url = "https://telegra.ph/Privacy-Policy-for-Pet-Routine-05-01",
+        url = "https://telegra.ph/Privacy-Policy-for-Road-BlazzeGo-05-25",
     ),
 )
